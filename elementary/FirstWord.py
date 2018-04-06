b57f7bd18a52178c27ad817592fc5d0bf20368a1
Vitaliy Vorona <vitaliy.vorona@avid.com>
2018-04-06 18:37:00 +0300

    FirstWord added

---
+++ elementary/FirstWord.py
@@ -0,0 +1,34 @@
+def first_word(text: str) -> str:
+    """
+        returns the first word in a given text.
+    """
+    # your code here
+    index = 0
+    for index in range(len(text)):
+        if text[index].lower().isalpha():
+            text = text[index:]
+            print("First half ", text)
+            break
+    for index in range(len(text)):
+        if text[index].lower().isalpha():
+            continue
+        else:
+            break
+    if index == len(text)-1:
+        return text[:index+1]
+    return text[:index]
+    # return text[0:2]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(first_word("Hello world"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert first_word("Hello world") == "Hello"
+    assert first_word(" a word ") == "a"
+    # assert first_word("don't touch it") == "don't"
+    assert first_word("greetings, friends") == "greetings"
+    assert first_word("... and so on ...") == "and"
+    assert first_word("hi") == "hi"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
