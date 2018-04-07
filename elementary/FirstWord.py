2063cfbf912ceddfd01982991eba7041840b0db7
VitaliyVorona <voronchik84@gmail.com>
2018-04-07 08:00:46 +0300

    FirstWord modified

--- elementary/FirstWord.py
+++ elementary/FirstWord.py
@@ -5,19 +5,21 @@ def first_word(text: str) -> str:
     # your code here
     index = 0
     for index in range(len(text)):
-        if text[index].lower().isalpha():
+        if text[index].isalpha():
             text = text[index:]
-            print("First half ", text)
             break
+
     for index in range(len(text)):
-        if text[index].lower().isalpha():
+        if text[index].isalpha() or text[index] == "'":
             continue
         else:
             break
+
     if index == len(text)-1:
         return text[:index+1]
     return text[:index]
-    # return text[0:2]
+
+
 
 
 if __name__ == '__main__':
@@ -27,7 +29,7 @@ if __name__ == '__main__':
     # These "asserts" are used for self-checking and not for an auto-testing
     assert first_word("Hello world") == "Hello"
     assert first_word(" a word ") == "a"
-    # assert first_word("don't touch it") == "don't"
+    assert first_word("don't touch it") == "don't"
     assert first_word("greetings, friends") == "greetings"
     assert first_word("... and so on ...") == "and"
     assert first_word("hi") == "hi"
