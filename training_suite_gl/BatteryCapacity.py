5473355e55d77721168a9e7d96a67b79d22b75e1
Vitaliy Vorona <vitaliy.vorona@avid.com>
2018-11-26 13:50:46 +0200

    Battery capacity and anagram detection in training_suite_gl are added

---
+++ training_suite_gl/BatteryCapacity.py
@@ -0,0 +1,26 @@
+import json
+
+data = '''
+        "SuperMaxCapacity" =0
+        "MaxCapacity": +4540;
+        'CurrentCapacity'=   2897,
+        "LegacyBatteryInfo" = {"Amperage"=18446744073709550521,"Flags"=4,"Capacity"=4540,"Current"=2897,"Voltage"=7283,"Cycle Count"=406}
+        "MegaMaxCapacity" = 6700
+'''
+
+
+def get_battery_level(data):
+    index_m = data.find('"MaxCapacity": +') + len('"MaxCapacity": +')
+    index_c = data.find("'CurrentCapacity'=   ") + len("'CurrentCapacity'=   ")
+
+    max_capacity = int(data[index_m:index_m + 4])
+    curr_capacity = int(data[index_c:index_c + 4])
+
+    percentage = round(100.0 * (curr_capacity / max_capacity), 2)
+
+    return str(percentage) + '%'
+
+
+'CurrentCapacity / MaxCapacity'
+
+print(get_battery_level(data))
